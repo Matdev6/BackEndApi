2b7736127036f2444bf62011eaa3c009311e230c
Matdev6 <mateusousa123@hotmail.com>
2025-02-03 16:52:12 -0300

    Server.js Cors
    Server.js Cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ import authMiddleware from "./middlewares/auth.js"
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 app.use(express.json());
 app.use(routes); // Usa as rotas sem o prefixo /api
 app.use(authMiddleware)
