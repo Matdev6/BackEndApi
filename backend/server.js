ca32f11e3efeb94c4acd9ee58669b66698788b82
Matdev6 <mateusousa123@hotmail.com>
2025-02-05 17:57:09 -0300

    CORS
    CORS

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,7 @@ import authMiddleware from "./middlewares/auth.js"
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000", // Permite apenas o frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-  credentials: true // Permite cookies e cabeçalhos de autenticação
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(routes); // Usa as rotas sem o prefixo /api
