615fbdaee2c5b761575720e5fd73bcf4bd26c103
Matdev6 <mateusousa123@hotmail.com>
2025-02-06 09:14:48 -0300

    Aplica o AuthMiddlaware antes da rota
    Aplica o AuthMiddlaware antes da rota

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
-app.use(routes); // Usa as rotas sem o prefixo /api
 app.use(authMiddleware)
-
+app.use(routes); // Usa as rotas sem o prefixo /api
 
 
 app.listen(port, () => {
